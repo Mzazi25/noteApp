7eef073fc6274c7cf22fc617cc3b945e537b52f5
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-05-22 09:25:58 +0300

    functional noteApp in Java

---
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -0,0 +1,83 @@
+package com.example.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.notekeeper.databinding.ActivityNoteListsBinding;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityNoteListsBinding binding;
+    private ArrayAdapter<NoteInfo> mAdapterNotes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityNoteListsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_note_list2);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NoteListActivity.this, MainActivity.class));
+            }
+        });
+
+        initializeDisplayContent();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapterNotes.notifyDataSetChanged();
+    }
+
+    private void initializeDisplayContent() {
+        final ListView listNotes = findViewById(R.id.lis_notes);
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+
+        listNotes.setAdapter(mAdapterNotes);
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(NoteListActivity.this, MainActivity.class
+                );
+//                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+                intent.putExtra(MainActivity.NOTE_POSITION,position);
+
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_note_list2);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
